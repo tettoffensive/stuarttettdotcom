191e93785473f3dbc9fee4b79a08833de165e2b7
Stuart Tett <tettoffensive@users.noreply.github.com>
2018-10-18 13:13:44 -0700

    Gulpify (#4)

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,21 @@
-var gulp = require('gulp');
-// Requires the gulp-sass plugin
+var gulp = require('gulp'),
+    babel = require('gulp-babel');
+
 var sass = require('gulp-sass');
 var haml = require('gulp-ruby-haml');
+
+var htmltidy = require('gulp-htmltidy');
+
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+
+
+var del = require('del');
 
 gulp.task('sass', function(){
   return gulp.src('public/**/*.scss')
@@ -11,7 +25,7 @@ gulp.task('sass', function(){
 
 gulp.task('haml', function () {
   gulp.src('public/**/*.haml')
-    .pipe(haml({format: 'xhtml'}))
+    .pipe(haml())
     .pipe(gulp.dest('public'));
 });
 
@@ -21,7 +35,64 @@ gulp.task('watch', function(){
   gulp.watch('public/**/*.haml', ['haml']);
 });
 
+gulp.task('_useref', function(){
+  return gulp.src('public/index.html')
+    .pipe(useref())
+    // Minifies only if it's a JavaScript file
+    .pipe(gulpIf('*.js', babel({presets: ['minify']})))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('useref', ['haml','sass','_useref'], function(){
+  return gulp.src('public/*/index.html')
+    .pipe(useref({noAssets: true}))
+    // Minifies only if it's a JavaScript file
+    // .pipe(gulpIf('*.html', htmltidy({outputXhtml: true})))
+    // .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+// gulp.task('images', function(){
+//   return gulp.src('public/images/**/*.+(png|jpg|gif|svg)')
+//   .pipe(imagemin())
+//   // Caching images that ran through imagemin
+//   .pipe(cache(imagemin({
+//       interlaced: true
+//     })))
+//   .pipe(gulp.dest('dist/images'))
+// });
+
+gulp.task('polymer', function() {
+  return gulp.src('public/src/**/*')
+  .pipe(gulp.dest('dist/src'))
+})
+
+gulp.task('images', function() {
+  return gulp.src('public/images/**/*')
+  .pipe(gulp.dest('dist/images'))
+})
+
+gulp.task('webfonts', function() {
+  return gulp.src('public/stylesheets/webfonts/**/*')
+  .pipe(gulp.dest('dist/stylesheets/webfonts'))
+})
+
+gulp.task('bower', function() {
+  return gulp.src('public/bower_components/**/*')
+  .pipe(gulp.dest('dist/bower_components'))
+})
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
+
 gulp.task('build', [
  'sass',
- 'haml'
+ 'haml',
+ 'useref',
+ 'webfonts',
+ 'images',
+ 'polymer',
+ 'bower'
 ]);
--- public/README.md
+++ public/README.md
@@ -2,6 +2,13 @@
 
 Personal Website
 
+## Gulp
+
+```
+$ gulp build
+$ gulp watch
+```
+
 ## Install the Polymer-CLI
 
 First, make sure you have the [Polymer CLI](https://www.npmjs.com/package/polymer-cli) installed. Then run `polymer serve` to serve your application locally.
@@ -9,24 +16,28 @@ First, make sure you have the [Polymer CLI](https://www.npmjs.com/package/polyme
 ## Viewing Your Application
 
 ```
+$ cd dist
 $ polymer serve
 ```
 
 ## Building Your Application
 
 ```
+$ cd dist
 $ polymer build
 ```
 
 This will create builds of your application in the `build/` directory, optimized to be served in production. You can then serve the built versions by giving `polymer serve` a folder to serve from:
 
 ```
+$ cd dist
 $ polymer serve build/default
 ```
 
 ## Running Tests
 
 ```
+$ cd dist
 $ polymer test
 ```
 
