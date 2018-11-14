8a985a4620efcb76e4a6cba17dac39878453a981
Stuart Tett <stuart.tett@gmail.com>
2018-11-14 12:36:57 -0800

    Setup contact form

--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -3,3 +3,19 @@ var forEach = function (array, callback, scope) {
     callback.call(scope, i, array[i]); // passes back stuff we need
   }
 }
+
+/**
+ * returns the width of child text of any DOM node as a float
+ */
+function getTextWidth(el) {
+  // uses a cached canvas if available
+  var canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
+  var context = canvas.getContext("2d");
+  // get the full font style property
+  var font = window.getComputedStyle(el, null).getPropertyValue('font');
+  var text = el.value;
+  // set the font attr for the canvas text
+  context.font = font;
+  var textMeasurement = context.measureText(text);
+  return textMeasurement.width;
+}
