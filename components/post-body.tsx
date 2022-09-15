3fa40ab333f274adda21391428a6b7c597362414
tettoffensive <stuart.tett@gmail.com>
2022-09-15 15:52:30 -0700

    refactor: switch to react-markdown
    - safer than dangerously set inner html, and allows using next/image
    - downside is bundle size a little bigger

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -1,4 +1,7 @@
+import ReactMarkdown from 'react-markdown'
+import remarkHtml from 'remark-html'
 import markdownStyles from './markdown-styles.module.css'
+import Image from 'next/image'
 
 type Props = {
   content: string
@@ -7,10 +10,28 @@ type Props = {
 const PostBody = ({ content }: Props) => {
   return (
     <div className="max-w-2xl mx-auto">
-      <div
-        className={markdownStyles['markdown']}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
+      <ReactMarkdown
+      className={markdownStyles['markdown']}
+    remarkPlugins={remarkHtml}
+    components={{
+        img: ({ node, ...props }) => {
+            const imageUrl = new URL(`https://${props.src}`);
+            const queryParams = new URLSearchParams(imageUrl.search)
+            const width = queryParams.get('width');
+            const height = queryParams.get('height');
+
+            return (
+              <Image
+                  src={props.src}
+                  alt={props.alt}
+                  layout="fill"
+              />
+            )
+        }
+    }}
+>
+    {content}
+</ReactMarkdown>
     </div>
   )
 }
